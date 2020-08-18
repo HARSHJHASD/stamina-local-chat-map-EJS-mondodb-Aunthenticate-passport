b15d6ded9f28d0bc1567920bfcacf91fa301dcf4
Harsh Jha <HARSHJHASD@users.noreply.github.com>
2020-08-18 20:50:09 +0530

    local/chat/map/ejs/mongodb/passport/login/singup/session/cookies

--- app.js
+++ app.js
@@ -2,20 +2,16 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 const saltRound = 10;
-
-
 const bodyparser = require("body-parser");
 const ejs = require("ejs");
-// contacting Mongoose
-mongoose.connect("mongodb://localhost:27017/staminaDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-mongoose.set("useCreateIndex", true);
 const _ = require("lodash")
 const app = express();
+
+
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
@@ -23,88 +19,123 @@ app.use(express.static("public"));
 app.use(bodyparser.urlencoded({
   extended: true
 }));
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+// contacting Mongoose
+mongoose.connect("mongodb://localhost:27017/staminaDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.set("useCreateIndex", true);
 
 var array = [];
 // creating schema
-var userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
-// creating model
+// adding plugin to our user schema
+userSchema.plugin(passportLocalMongoose);
+// creating model from userschema
 const User = mongoose.model("User", userSchema);
-
-
+// use static authenticate method of model in LocalStrategy
+passport.use(User.createStrategy());
+// here we are adding to our "model";"user"
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 
 app.post("/register", function(req, res) {
-
-  bcrypt.hash(req.body.password, saltRound, function(error, hash) {
-    const user = new User({
-      email: req.body.username,
-      password: hash
-    });
-
-    array.push(user);
-    User.insertMany(array, function(error, result) {
-      if (error) {
-        console.log(error);
-      } else {
-        res.redirect("/login");
-      }
-    });
+  User.register({username: req.body.username}, req.body.password, function(err, user){
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/index");
+      });
+    }
   });
-
 });
 
 
 app.post("/login", function(req, res) {
-      const password = req.body.password;
-      User.findOne({
-          email: req.body.username
-        }, function(error, founduser) {
-          if (error) {
-            console.log(error);
-          } else {
-            if (founduser) {
-              bcrypt.compare(password, founduser.password, function(error, result) {
-                if (result===true) {
-                  res.render("index.ejs");
-                } else {
-                  res.redirect("/login");
-                }
-              });
-            } else {
-              res.redirect("/register");
-            }
-          }
-      });
+  const user = new User({
+      username: req.body.username,
+      password: req.body.password
     });
 
-    app.get("/register", function(req, res) {
-      res.render("register.ejs");
-    }); app.get("/login", function(req, res) {
-      res.render("login.ejs");
-    }); app.get("/", function(req, res) {
-      res.render("home.ejs");
-    }); app.get("/about", function(req, res) {
-      res.render("about.ejs");
-    }); app.get("/blog", function(req, res) {
-      res.render("blog.ejs");
-    }); app.get("/contact", function(req, res) {
-      res.render("contact.ejs");
-    }); app.get("/gallery", function(req, res) {
-      res.render("gallery.ejs");
-    }); app.get("/pricing", function(req, res) {
-      res.render("pricing.ejs");
-    }); app.get("/weightgaining", function(req, res) {
-      res.render("weightgaining.ejs");
-    }); app.get("/weighttraining", function(req, res) {
-      res.render("weighttraining.ejs");
-    }); app.get("/weightloss", function(req, res) {
-      res.render("weightloss.ejs");
+    req.login(user, function(err){
+      if (err) {
+        console.log(err);
+      } else {
+        passport.authenticate("local")(req, res, function(){
+          res.redirect("/index");
+        });
+      }
     });
+});
 
 
-    app.listen(3000, function() {
-      console.log("Server started on port 3000");
-    });
+app.get("/logout",function(req,res){
+req.logout();
+res.redirect("/login");
+});
+
+
+app.get("/index", function(req, res) {
+  if(req.isAuthenticated()){
+    res.render("index.ejs");
+  }else{
+    res.redirect("/login");
+  }
+});
+
+app.get("/register", function(req, res) {
+  res.render("register.ejs");
+});
+app.get("/login", function(req, res) {
+  res.render("login.ejs");
+});
+app.get("/", function(req, res) {
+  res.render("home.ejs");
+});
+app.get("/about", function(req, res) {
+  res.render("about.ejs");
+});
+app.get("/blog", function(req, res) {
+  res.render("blog.ejs");
+});
+app.get("/contact", function(req, res) {
+  res.render("contact.ejs");
+});
+app.get("/gallery", function(req, res) {
+  res.render("gallery.ejs");
+});
+app.get("/pricing", function(req, res) {
+  res.render("pricing.ejs");
+});
+app.get("/weightgaining", function(req, res) {
+  res.render("weightgaining.ejs");
+});
+app.get("/weighttraining", function(req, res) {
+  res.render("weighttraining.ejs");
+});
+app.get("/weightloss", function(req, res) {
+  res.render("weightloss.ejs");
+});
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
