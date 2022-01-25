5fecbc08eaf205eb14a7ff4e86a1eebd61d98f4e
Harsh Jha <harshjha19101997@gmail.com>
2022-01-25 12:34:20 +0530

    interview ready

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//(npm i dotenv) this is for security  purpose ,as we have added a .env file and inside that we have stored our apis and secret files so for that
 require("dotenv").config();
 //jshint esversion:6
 //requring express(npm module) to setup our server
@@ -5,8 +6,11 @@ const express = require("express");
 //mongoose is a module in npm using whihc we can connect our website to mongodb data vase easily..(we have other ay MONGODB NATIVE DRIVER TOO)
 const mongoose = require("mongoose");
 const session = require("express-session");
+//npm package that will be used for encryption ,authentication.
 const passport = require("passport");
+//requiring passport local mongooose(for authentification)
 const passportLocalMongoose = require("passport-local-mongoose");
+//used for salting.
 const saltRound = 10;
 //BODY parser is used so that we can use the form data in our app.js r in nodejs part
 const bodyparser = require("body-parser");
@@ -18,6 +22,7 @@ const _ = require("lodash")
 const app = express();
 //here we are setting view engine as ejs, this is what we did after npm ejs module from npm ..this is compulsory if u r suing ejs
 app.set("view engine", "ejs");
+//al static files inside ou public folder
 app.use(express.static("public"));
 //in order to give static files (images and css) in node js we will  use static folder named "public"
 // using body parser to fetch the code
@@ -25,15 +30,15 @@ app.use(bodyparser.urlencoded({
   extended: true
 }));
 
-
-
+//to setup session through cookies etc
 app.use(session({
+  //to access env variable stored  inside our .env "process.env.SECRET"
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: false,
 }));
-
 app.use(passport.initialize());
+//using passport and session with express.
 app.use(passport.session());
 // contacting Mongoose(this is how we connect to our database using mongooose)
 // mongoose.connect("mongodb://localhost:27017/staminaDB", {
@@ -41,11 +46,12 @@ app.use(passport.session());
 //   useUnifiedTopology: true
 // });
 
+//we are adding mongodb database using mongoose
 mongoose.connect("mongodb+srv://admin-harsh:Test123@cluster0.whpfi.mongodb.net/staminaDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
-
+//mandatory thing with mongoose
 mongoose.set("useCreateIndex", true);
 
 //we have created an empty array
@@ -61,7 +67,7 @@ userSchema.plugin(passportLocalMongoose);
 const User = mongoose.model("User", userSchema);
 // use static authenticate method of model in LocalStrategy
 passport.use(User.createStrategy());
-// here we are adding to our "model";"user"
+// here we are adding passport to our "model";"user"
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -69,10 +75,12 @@ passport.deserializeUser(User.deserializeUser());
 app.post("/register", function(req, res) {
   User.register({username: req.body.username}, req.body.password, function(err, user){
     if (err) {
+      //error here cud be "user already in databasee" and that ud be printed on console .log
       console.log(err);
-      //if error then again redirect to login page
+      //if error(old user) then again redirect to signup page
       res.redirect("/register");
     } else {
+      //if user is isAuthenticated then proceed him/her to /index.
       passport.authenticate("local")(req, res, function(){
         res.redirect("/index");
       });
@@ -87,7 +95,6 @@ app.post("/login", function(req, res) {
       username: req.body.username,
       password: req.body.password
     });
-
     req.login(user, function(err){
       if (err) {
         console.log(err);
@@ -105,7 +112,7 @@ req.logout();
 res.redirect("/login");
 });
 
-
+//if anyone tried to get to /index directly ,it wil be checked if he/she is
 app.get("/index", function(req, res) {
   if(req.isAuthenticated()){
     res.render("index.ejs");
